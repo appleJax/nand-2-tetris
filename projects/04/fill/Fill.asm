05264a49a0557a70d4c7d9ef7dfb5fb4a059bcd7
appleJax <appleJax@users.noreply.github.com>
2019-12-30 11:14:51 -0800

    project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,56 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+  @SCREEN // initialize @position to start of screen memory map
+  D=A
+  @position
+  M=D
+
+  @8192 // initialize counter to number of registers in screen map (256 x (512 / 16))
+  D=A
+  @i
+  M=D
+
+(DRAW_LOOP)
+  @READ_KBD
+  0;JMP
+
+(DRAW_REGISTER)
+  @toggle_value // use -1 or 0 depending on KBD input
+  D=M
+  @position
+  A=M
+  M=D
+
+  @position // increment position, decrement counter
+  M=M+1
+  @i
+  MD=M-1
+
+  @START // when counter gets to 0, we are at the end of the screen memory map, so start over
+  D;JLE
+
+  @DRAW_LOOP
+  0;JMP
+
+(READ_KBD)
+  @KBD
+  D=M
+  @TOGGLE_OFF
+  D;JEQ
+  @TOGGLE_ON
+  0;JMP
+
+(TOGGLE_ON)
+  @toggle_value
+  M=-1
+  @DRAW_REGISTER
+  0;JMP
+
+(TOGGLE_OFF)
+  @toggle_value
+  M=0
+  @DRAW_REGISTER
+  0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2 // initialize result to 0
+M=0
+
+@R0 // copy multiplier to different address to avoid mutating the input
+D=M
+@i
+M=D
+
+@END
+D;JEQ
+
+(LOOP)
+@R1
+D=M
+@R2
+M=D+M
+
+@i
+MD=M-1
+@END
+D;JLE
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
